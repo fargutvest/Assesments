545d1faf2d35ef99c832703443cc216228f54764
Farragut West <fargutvest@gmail.com>
2023-08-05 18:07:23 +0300

    Added logic that calculates shades of green color, and removed InputData struct

---
+++ AxonSoft.Assesment/AxonSoft.Assesment/ColorsHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace AxonSoft.Assesment
+{
+    public static class ColorsHelper
+    {
+        public static Color[] GetAllShadesOfGreen()
+        {
+            List<Color> allShadesOfGreen = new List<Color>();
+
+            int length = 255;
+            int step = 5;
+            for (int r = 0; r < length; r += step)
+            {
+                for (int g = 0; g < length; g += step)
+                {
+                    for (int b = 0; b < length; b += step)
+                    {
+                        Color color = Color.FromArgb(r, g, b);
+                        float hue = color.GetHue();
+                        float brightness = color.GetBrightness();
+                        float saturation = color.GetSaturation();
+
+                        if (hue > 100 && hue < 150 && brightness > 0.1 && brightness < 0.8)
+                        {
+                            allShadesOfGreen.Add(color);
+                        }
+                    }
+                }
+            }
+
+            var ordered = allShadesOfGreen.OrderByDescending(_ => _.GetBrightness()).ToArray();
+
+            var result = new List<Color>();
+            for (int i = 0; i < ordered.Length; i += 100)
+            {
+                result.Add(ordered[i]);
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
